ce05039a6b15826d3b78f61cb6156ad7150c153d
Dominik Hanuš <hanydominik@seznam.cz>
2026-05-24 20:47:46 +0200

    fix: oprava hesla testovacích uživatelů

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT IGNORE INTO users (name, email, password, role) VALUES
-('Dominik', 'dominik@seznam.cz', '$2a$12$LL6jHTOW5XBRSqyC50gU6.ZUvn0NkzxIwo09eSiPnqJGHerErGsRW', 'USER'),
-('Petr', 'petr@seznam.cz', '$2a$12$LL6jHTOW5XBRSqyC50gU6.ZUvn0NkzxIwo09eSiPnqJGHerErGsRW', 'APPROVER'),
-('Filip', 'filip@seznam.cz', '$2a$12$LL6jHTOW5XBRSqyC50gU6.ZUvn0NkzxIwo09eSiPnqJGHerErGsRW', 'ADMIN');
+('Dominik', 'dominik@seznam.cz', '$2a$10$BfameRj0/KbLj1dSmjm1fO.q.Z.eRxlNHZnA3mTmhr45AvPt0.wzC', 'USER'),
+('Petr', 'petr@seznam.cz', '$2a$10$BfameRj0/KbLj1dSmjm1fO.q.Z.eRxlNHZnA3mTmhr45AvPt0.wzC', 'APPROVER'),
+('Filip', 'filip@seznam.cz', '$2a$10$BfameRj0/KbLj1dSmjm1fO.q.Z.eRxlNHZnA3mTmhr45AvPt0.wzC', 'ADMIN');
