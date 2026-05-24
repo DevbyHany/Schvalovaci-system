b487a042c868bf0a02cc281bdb87465bdab14c9c
Dominik Hanuš <hanydominik@seznam.cz>
2026-05-24 20:10:53 +0200

    feat: přidání testovacích uživatelů při startu aplikace

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT IGNORE INTO users (name, email, password, role) VALUES
+('Dominik', 'dominik@seznam.cz', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LnQ5HTLW79a', 'USER'),
+('Petr', 'petr@seznam.cz', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LnQ5HTLW79a', 'APPROVER'),
+('Filip', 'filip@seznam.cz', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LnQ5HTLW79a', 'ADMIN');
