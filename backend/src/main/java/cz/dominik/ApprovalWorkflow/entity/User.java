1ac623f9249a011b2022884bb252326970ac6c77
Dominik Hanuš <hanydominik@seznam.cz>
2026-05-25 17:05:16 +0200

    .

--- backend/src/main/java/cz/dominik/ApprovalWorkflow/entity/User.java
+++ backend/src/main/java/cz/dominik/ApprovalWorkflow/entity/User.java
@@ -32,6 +32,7 @@ public class User implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     @Email
     private String email;
 
