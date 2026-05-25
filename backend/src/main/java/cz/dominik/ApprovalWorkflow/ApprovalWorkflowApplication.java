f5a0052bbc2a26c7fb2c3ffa430a8e8a769f8549
Dominik Hanuš <hanydominik@seznam.cz>
2026-05-25 16:24:22 +0200

    změna hesla do databáze

--- backend/src/main/java/cz/dominik/ApprovalWorkflow/ApprovalWorkflowApplication.java
+++ backend/src/main/java/cz/dominik/ApprovalWorkflow/ApprovalWorkflowApplication.java
@@ -2,11 +2,13 @@ package cz.dominik.ApprovalWorkflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ApprovalWorkflowApplication {
 
     public static void main(String[] args) {
+        System.out.println(new BCryptPasswordEncoder().encode("heslo123"));
         SpringApplication.run(ApprovalWorkflowApplication.class, args);
     }
 
