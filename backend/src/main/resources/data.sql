6d97fed2be430b1eaa05840760acf32b065db1ad
Dominik Hanuš <hanydominik@seznam.cz>
2026-06-28 18:36:44 +0200

    .

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,4 +1,5 @@
 INSERT IGNORE INTO users (name, email, password, role) VALUES
 ('Dominik', 'dominik@seznam.cz', '$2a$10$BfameRj0/KbLj1dSmjm1fO.q.Z.eRxlNHZnA3mTmhr45AvPt0.wzC', 'USER'),
+('Tomáš', 'tomas@seznam.cz', '$2a$10$BfameRj0/KbLj1dSmjm1fO.q.Z.eRxlNHZnA3mTmhr45AvPt0.wzC', 'APPROVER'),
 ('Petr', 'petr@seznam.cz', '$2a$10$BfameRj0/KbLj1dSmjm1fO.q.Z.eRxlNHZnA3mTmhr45AvPt0.wzC', 'APPROVER'),
 ('Filip', 'filip@seznam.cz', '$2a$10$BfameRj0/KbLj1dSmjm1fO.q.Z.eRxlNHZnA3mTmhr45AvPt0.wzC', 'ADMIN');
